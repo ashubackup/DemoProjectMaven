9508780c214b014d840e056cb006a30540d8334d
unknown <ashubackup>
2024-03-21 18:35:23 +0530

    DemoProjectMaven

---
+++ target/classes/gen/MyThread.java
@@ -0,0 +1,7 @@
+package gen;
+public class MyThread extends Thread {
+    public void run() {
+        // Code to be executed by the thread
+        System.out.println("MyThread is running...");
+    }
+}
